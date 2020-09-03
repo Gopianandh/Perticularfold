9d61f6156dde20bd5aa68b3e2f630d83fcfa2d1c
Gopianandh <gopinathan2296@gmail.com>
2020-09-03 21:50:11 +0530

    done my code

---
+++ src/test/java/org/gi/Gitrepots2.java
@@ -0,0 +1,14 @@
+package org.gi;
+
+public class Gitrepots2 {
+	private void samp() {
+		
+		System.out.println("sample ");
+	}
+		public static void main(String[] args) {
+			
+			Gitrepots2 re=new  Gitrepots2();
+			 re.samp();
+		}
+
+}
